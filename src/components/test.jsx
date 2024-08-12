e0d7529e00d87359aba08f35baf48e3a17c70f3a
Chao Lobi <gogoiladitya@gmail.com>
2024-08-12 17:25:20 +0530

    tailwind setup

---
+++ src/components/test.jsx
@@ -0,0 +1,17 @@
+const Test = () => {
+  return (
+    <div className="w-96 max-h-full justify-center items-center m-auto">
+      <div className="w-48 h-48 bg-green-500 hover:scale-150 transition-transform duration-300">
+        ExamAce | JEE
+      </div>
+      <div className="w-48 h-48 bg-green-500 hover:scale-150 transition-transform duration-300">
+        ExamAce | JEE
+      </div>
+      <div className="w-48 h-48 bg-green-500 hover:scale-150 transition-transform duration-300">
+        ExamAce | JEE
+      </div>
+    </div>
+  );
+};
+
+export default Test;
